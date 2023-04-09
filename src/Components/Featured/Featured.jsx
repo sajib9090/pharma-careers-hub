9a10a01e2e5b1125261a29a97260c6889ee65f4a
sajib9090 <nispap.devil.sajib@gmail.com>
2023-04-10 01:49:32 +0600

    fetch job and see all job button dynamically fixed

--- src/Components/Featured/Featured.jsx
+++ src/Components/Featured/Featured.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import SingleJobCard from "./SingleJobCard/SingleJobCard";
 
 const Featured = () => {
-
   const jobs = useLoaderData();
+
+  const [seeAll, setSeeAll] = useState(false);
+
+  const handleSeeAllBtn = () => {
+    setSeeAll(true);
+  }
   return (
-    <div className="mt-32">
+    <div className="mt-32 max-w-7xl mx-auto px-4 md:px-8">
       <div className="text-center">
         <h1 className="text-[#1A1919] text-4xl mb-4 font-extrabold">Featured Jobs</h1>
         <p className="text-[#757575]">
@@ -14,10 +19,15 @@ const Featured = () => {
           need. Its your future
         </p>
       </div>
-      <div>
+      <div className="grid md:grid-cols-2 gap-6">
         {
-           jobs.map(job => <SingleJobCard key = {job.id} job = {job}/>)
+           jobs?.slice(0, seeAll? 6 : 4).map(job => <SingleJobCard key = {job.id} job = {job}/>)
         }
+      </div>
+      <div className="text-center mt-10">
+         { !seeAll && 
+            <button onClick={handleSeeAllBtn} className="btn capitalize bg-gradient-to-r from-[#9873FF] border-none px-8">See All Jobs</button>
+         }
       </div>
     </div>
   );
--- src/Components/Featured/SingleJobCard/SingleJobCard.jsx
+++ src/Components/Featured/SingleJobCard/SingleJobCard.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid'
 
 const SingleJobCard = ({job}) => {
-    console.log(job);
+    const {img, title, id, brand, job_category, job_type, location, salary
+    } = job;
     return (
-        <div>
+        <div className='border border-[#E8E8E8] rounded-lg mt-8 h-[360px] flex flex-col justify-center pl-10'>
+            <div>
+                <img className='w-[150px] h-[50px]' src={img} alt="" />
+            </div>
+            <div>
+                <h4 className='text-[#474747] font-bold text-xl mt-6 mb-2'>{title}</h4>
+                <p className='text-[#757575] mt-2 mb-3'>{brand}</p>
+            </div>
+            <div className='mb-4'>
+                <button className="border border-[#7E90FE] px-4 py-2 mr-4 rounded-lg text-[#7E90FE]">{job_type}</button>
+                <button className="border border-[#7E90FE] px-4 py-2 mr-4 rounded-lg  text-[#7E90FE]">{job_category}</button>
+            </div>
+            <div className='flex items-center'>
+                <MapPinIcon className='h-6 w-6 text-[#757575]'/> <address className='mr-6'>{location}</address>
+                <CurrencyDollarIcon className="h-6 w-6 text-[#757575]" /> <p>{salary}</p>
+            </div>
+            <div className='mt-6 mb-10'>
+                <button className="btn capitalize bg-gradient-to-r from-[#9873FF] border-none">View Details</button>
+            </div>
+
             
         </div>
     );
